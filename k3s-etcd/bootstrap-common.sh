51d6e6886f74da4432dc6c9072d35ff0df3f51df
Alberto Rodrguez Garcia <agarcia@schubergphilis.com>
2021-11-16 17:08:09 +0100

    WIP

--- k3s-etcd/bootstrap-common.sh
+++ k3s-etcd/bootstrap-common.sh
@@ -12,8 +12,7 @@ systemctl stop ufw.service
 systemctl disable ufw.service
 whoami
 echo 'export INSTALL_K3S_VERSION=v1.21.1' >>~/.bashrc
-export INSTALL_K3S_VERSION=v1.21.1
-INSTALL_K3S_VERSION=v1.21.1 ; curl -sfL https://get.k3s.io -o /usr/local/bin/k3s
+curl -sfL https://github.com/k3s-io/k3s/releases/download/v1.21.1%2Bk3s1/k3s -o /usr/local/bin/k3s
 chmod 755 /usr/local/bin/k3s
 curl -sLO "https://dl.k8s.io/release/$(curl -L -s https://dl.k8s.io/release/stable.txt)/bin/linux/amd64/kubectl"
 install -o root -g root -m 0755 kubectl /usr/local/bin/kubectl
